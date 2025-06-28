fbeeb0b3b87c1229c16a170b4dec22f216053e13
Martin <17749252+martin-mok@users.noreply.github.com>
2025-06-28 12:25:11 +0100

    output jest coverage to terminal

--- jest.config.ts
+++ jest.config.ts
@@ -11,6 +11,7 @@ const config: Config = {
   },
   testRegex: '(/test/.*|(\\.|/)(test|spec))\\.(m)?ts$',
   coverageDirectory: 'coverage',
+  collectCoverage: true,
   collectCoverageFrom: [
     'src/**/*.ts',
     'src/**/*.mts',
